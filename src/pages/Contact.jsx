cd3b13e30f46adc71ef8b551dbd6faecbfd7ba11
Hikmot-O <hikmotokunola@gmail.com>
2023-04-30 20:40:17 +0100

    animation fix

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,6 +5,10 @@ import Header from "../components/Header";
 const Contact = () => {
   return (
     <section className=" h-screen bg-fuchsia-100 px-[30px] pt-10 overflow-y-auto">
+      <div className="flex flex-col sm:flex-row absolute">
+        <div className="animate-blob_1 h-[50px]  w-[100px] sm:w-[500px] sm:h-[100%] top-[100px] filter blur-3xl left-10 mix-blend-multiply bg-purple-300 rounded-full "></div>
+        <div className="animate-blob h-[50px]   w-[100px] sm:w-[500px] sm:h-[100%] top-[100px] filter blur-3xl right-10 mix-blend-multiply bg-pink-300 rounded-full "></div>
+      </div>
       <Header />
 
       <div className="mt-[30px] sm:mt-[100px] grid grid-cols-1 sm:grid-cols-2 gap-y-[50px] sm:gap-x-[50px]">
@@ -33,7 +37,13 @@ const Contact = () => {
           </div>
 
           <div className="flex flex-col sm:flex-row gap-[25px] sm:gap-[50px] items-start sm:items-center">
-            <a href="https://instagram.com/hikmot" target='_blank' className="social_links">Instagram</a>
+            <a
+              href="https://instagram.com/hikmot"
+              target="_blank"
+              className="social_links"
+            >
+              Instagram
+            </a>
             <a
               href="https://github.com/Hikmot-O"
               target="_blank"
@@ -43,7 +53,7 @@ const Contact = () => {
             </a>
             <a
               href="https://www.linkedin.com/in/hikmot-okunola-816671187/"
-              target='_blank'
+              target="_blank"
               className="social_links"
             >
               LinkedIn
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,9 +5,9 @@ import NavBar from "../components/NavBar";
 
 const Home = () => {
   return (
-    <section className="relative pt-[30px] pb-[50px] only:sm:pt-10 p-5 flex flex-col items-center h-screen overflow-y-scroll w-full ">
-      <div className="animate-blob_1  absolute w-[500px] h-[500px] top-[100px] filter blur-3xl left-10 mix-blend-multiply bg-green-400 rounded-full "></div>
-      <div className="animate-blob  absolute w-[500px] h-[500px] top-[100px] filter blur-3xl right-10 mix-blend-multiply bg-blue-300 rounded-full "></div>
+    <section className="bg-fuchsia-100 relative pt-[30px] pb-[50px] only:sm:pt-10 p-5 flex flex-col items-center h-screen overflow-y-scroll w-full ">
+      <div className="animate-blob_1  absolute w-[500px] h-[100%] top-[100px] filter blur-3xl left-10 mix-blend-multiply bg-purple-300 rounded-full "></div>
+      <div className="animate-blob  absolute w-[500px] h-[100%] top-[100px] filter blur-3xl right-10 mix-blend-multiply bg-pink-300 rounded-full "></div>
       <Header />
       {/* <header className="w-full flex justify-between items-center">
         <h3 className="color-white">h!</h3>
