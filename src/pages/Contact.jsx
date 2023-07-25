522eb49a9f63c4dc479ca36b2aab8b21bdacb0cb
Hikmot-O <hikmotokunola@gmail.com>
2023-07-25 08:51:49 +0100

    update: mobile

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -13,21 +13,21 @@ const Contact = () => {
         <Header />
       </div>
 
-      <div className="mt-[68px] px-5 sm:mt-[100px] grid grid-cols-1 sm:grid-cols-2 gap-y-[50px] sm:gap-x-[50px]">
-        <h4 className="font-[600] text-[20px] sm:text-2xl md:text-[45px] md:leading-[50px]">
+      <div className="mt-[68px] px-5 sm:mt-[100px] grid grid-cols-1 sm:grid-cols-2 sm:gap-x-[50px]">
+        <h4 className="mb-12 md:mb-0 font-[600] text-[20px] sm:text-2xl md:text-[45px] md:leading-[50px]">
           Have an idea or concept that really excites you? Let's make magic
           together!
         </h4>
 
         <div className="">
-          <div className="mb-[40px]">
+          <div className="mb-6 md:mb-[40px]">
             <p className="contact_label">Email</p>
             <a href="mailto:hikmotokunola@gmail.com" className="contact_main">
               hikmotokunola@gmail.com
             </a>
           </div>
 
-          <div className="mb-[60px]">
+          <div className="mb-10 md:mb-[60px]">
             <p className="contact_label">Twitter</p>
             <a
               href="https://twitter.com/HikmotAbolade"
@@ -38,7 +38,7 @@ const Contact = () => {
             </a>
           </div>
 
-          <div className="flex flex-col sm:flex-row gap-[25px] sm:gap-[50px] items-start sm:items-center">
+          <div className="flex flex-col sm:flex-row gap-[20px] sm:gap-[50px] items-start sm:items-center">
             <a
               href="https://instagram.com/hikmot"
               target="_blank"
