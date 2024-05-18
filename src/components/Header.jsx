185985dc89400d79d2e66dffd48f3b8aa60acf20
Hikmot-O <hikmotokunola@gmail.com>
2024-05-18 14:45:37 +0100

    update: download resume

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
       <Link to="/">
         <h3 className="color-white font-[600] text-[20px]">hikmot.</h3>
       </Link>
-      <a href="https://docs.google.com/document/d/1ZC3Y41uyGiELL3c4-LGSa0zgyt6iqpqPpll7QBV15UQ" download='Resume-Hikmot Okunola-FrontendEngineer' className="cursor-pointer text-[14.5px] border-[1px] border-black rounded-[20px] p-[10px] font-[500] ">
+      <a href="/CV/Hikmot-Okunola-Frontend-Engineer.pdf" target="_blank" download className="cursor-pointer text-[14.5px] border-[1px] border-black rounded-[20px] p-[10px] font-[500] ">
         Download Resume
       </a>
     </header>
